5fbd90c773d825a08db1cf6bc4896e37a63cbdff
meghatyagii <megha30.tyagi@gmail.com>
2017-06-28 23:38:38 -0700

    evolution of instabot

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+
+APP_ACCESS_TOKEN = '2177183342.405fc93.c82eb79788b248f2bfe5792b9d04e685'
+BASE_URL = 'https://api.instagram.com/v1/'
+
+def self_info():
+    request_url = (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'User does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+self_info()
+
+def get_user_id(the_chillvibe):
+def get_user_info(the_chillvibe):
+  user_id = get_user_id (the_chillvibe)
+  if user_id == None:
+    print 'User does not exist!'
+    exit()
+  request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+  print 'GET request url : %s' % (request_url)
+  user_info = requests.get(request_url).json()
+def get_user_id(the_chillvibe):
+
+  if user_info['meta']['code'] == 200:
+    if len(user_info['data']):
+      print 'Username: %s' % (user_info['data']['username'])
+      print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+      print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+      print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+    else:
+      print 'There is no data for this user!'
+  else:
+    print 'Status code other than 200 received!'
+get_user_info()
